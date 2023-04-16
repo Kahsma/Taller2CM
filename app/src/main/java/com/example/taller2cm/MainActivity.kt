24db3d38a0d5b347e387e0bdb3c820e92c530e17
Camilo Jose Martinez <cajomapa@gmail.com>
2023-04-16 00:51:44 -0500

    Galeria y Camara

--- app/src/main/java/com/example/taller2cm/MainActivity.kt
+++ app/src/main/java/com/example/taller2cm/MainActivity.kt
@@ -17,20 +17,47 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     companion object {
         private const val CONTACTS_PERMISSION_REQUEST_CODE = 100
+        private const val REQUEST_CAMERA_PERMISSION = 1
+        private const val REQUEST_STORAGE_PERMISSION = 2
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        contactsLogic()
+
         binding.contacts.setOnClickListener {
-            startActivity(Intent(baseContext,Contactsactivity::class.java))
+            contactsLogic()
 
-
+        }
+        binding.gallery.setOnClickListener {
+            galleryLogic()
         }
 
     }
+
+    private fun galleryLogic(){
+        if(ContextCompat.checkSelfPermission(
+                baseContext,
+                Manifest.permission.CAMERA
+        )== PackageManager.PERMISSION_GRANTED
+        ){
+            // Permission already granted, proceed with your logic
+            showToast("Camera permission already granted")
+            startActivity(Intent(baseContext,PhotoActivity::class.java))
+
+        }else if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
+            showCameraRationaleDialog()
+        }else{
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                REQUEST_CAMERA_PERMISSION
+            )
+        }
+    }
+
+
 
     private fun contactsLogic(){
         if (ContextCompat.checkSelfPermission(
@@ -40,10 +67,9 @@ class MainActivity : AppCompatActivity() {
         ) {
 
 
-
-
             // Permission already granted, proceed with your logic
             showToast("Contacts permission already granted")
+            startActivity(Intent(baseContext,Contactsactivity::class.java))
 
         } else if (shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS)) {
             // Permission denied, show rationale message and request permission again
@@ -60,20 +86,46 @@ class MainActivity : AppCompatActivity() {
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
-        permissions: Array<String>,
+        permissions: Array<out String>,
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == CONTACTS_PERMISSION_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission granted, proceed with your logic
-                showToast("Contacts permission granted")
-            } else {
-                // Permission not granted, handle the error or inform the user
-                showToast("Contacts permission denied")
+        when (requestCode) {
+            REQUEST_CAMERA_PERMISSION -> {
+                // Check if camera permission has been granted
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted, proceed with your logic
+                } else {
+                    // Permission not granted, handle the error or inform the user
+                }
+            }
+            REQUEST_STORAGE_PERMISSION -> {
+                // Check if storage permissions have been granted
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    // Permission granted, proceed with your logic
+                } else {
+                    // Permission not granted, handle the error or inform the user
+                }
+            }
+            CONTACTS_PERMISSION_REQUEST_CODE -> {
+                // Check if contacts permission has been granted
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted, proceed with your logic
+                } else {
+                    // Permission not granted, show rationale dialog or inform the user
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
+                        // Show rationale dialog
+                        showRationaleDialog()
+                    } else {
+                        // Permission permanently denied, handle the error or inform the user
+                    }
+                }
             }
         }
     }
+
     private fun showRationaleDialog() {
         Log.d("asd", "Showing rationale dialog")
         AlertDialog.Builder(this)
@@ -95,6 +147,28 @@ class MainActivity : AppCompatActivity() {
             .create()
             .show()
     }
+    private fun showCameraRationaleDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Camera Permission  Needed")
+            .setMessage("This app needs access to your camera to work properly.")
+            .setPositiveButton("OK"){_,_->
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.CAMERA),
+                    REQUEST_CAMERA_PERMISSION
+                )
+            }
+            .setNegativeButton("Cancel"){dialog,_ ->
+                dialog.dismiss()
+            }
+            .create()
+            .show()
+
+
+
+
+    }
+
 
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/example/taller2cm/PhotoActivity.kt
+++ app/src/main/java/com/example/taller2cm/PhotoActivity.kt
@@ -1,16 +1,48 @@
 package com.example.taller2cm
 
+import android.app.Activity
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import com.example.taller2cm.databinding.ActivityPhotoBinding
+import java.io.File
 
 class PhotoActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPhotoBinding
+    private lateinit var uriCamera: Uri
+
+    //request gallery
+    private val galleryrequest = registerForActivityResult(ActivityResultContracts.GetContent(), ActivityResultCallback { result: Uri? -> loadImage(result) })
+    private val cameraRequest = registerForActivityResult(ActivityResultContracts.TakePicture(), ActivityResultCallback { loadImage(uriCamera) })
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPhotoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        intialzeFile()
+        binding.galleryB.setOnClickListener {
+            galleryrequest.launch("image/*")
+        }
+        binding.cameraB.setOnClickListener {
+            cameraRequest.launch(uriCamera)
+        }
 
+
+
+    }
+
+    private fun intialzeFile() {
+        val file = File(filesDir,"picFromCamera")
+        uriCamera = FileProvider.getUriForFile(this, applicationContext.packageName+".fileprovider",file)
+    }
+
+    fun loadImage(result: Uri?){
+        val imageStream = contentResolver.openInputStream(result!!)
+        val image = BitmapFactory.decodeStream(imageStream)
+        binding.image.setImageBitmap(image)
 
     }
 }
